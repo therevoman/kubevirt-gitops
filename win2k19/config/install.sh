0c60c1d4c032951cb80c9aaba158a32a5596cd22
cwilkers <cwilkers@redhat.com>
2022-01-31 19:25:39 -0600

    Updated win2k19 to work with kube or ocp

--- win2k19/config/install.sh
+++ win2k19/config/install.sh
@@ -7,7 +7,7 @@ cd /scripts
 wget http://hyperconverged-cluster-cli-download.openshift-cnv.svc.cluster.local:8080/amd64/linux/virtctl.tar.gz -O /tmp/virtctl.tar.gz
 tar -xzvf /tmp/virtctl.tar.gz -C /tmp/
 
-oc apply -f windows-install-vm.yaml
+${KC} apply -f windows-install-vm.yaml
 echo "Applied VM"
 sleep 5
 
@@ -15,40 +15,40 @@ sleep 5
 echo "Started VM, waiting for VM to start"
 sleep 5
 
-vm_ready=$(oc get vm windows-install -o jsonpath='{.status.ready}')
+vm_ready=$(${KC} get vm windows-install -o jsonpath='{.status.ready}')
 while [ "$vm_ready" != "true" ]
 do
     sleep 10
-    vm_ready=$(oc get vm windows-install -o jsonpath='{.status.ready}')
+    vm_ready=$(${KC} get vm windows-install -o jsonpath='{.status.ready}')
 done
 
 echo "VM is started. Waiting for VMI to finish successfully."
-vmi_phase=$(oc get vmi windows-install -o jsonpath='{.status.phase}')
+vmi_phase=$(${KC} get vmi windows-install -o jsonpath='{.status.phase}')
 while [ "$vmi_phase" != "Succeeded" ]
 do
     sleep 30
-    vmi_phase=$(oc get vmi windows-install -o jsonpath='{.status.phase}')
+    vmi_phase=$(${KC} get vmi windows-install -o jsonpath='{.status.phase}')
     if [ "$vmi_phase" == "" ]; then
         vmi_phase="Succeeded"
     fi
 done
 
 echo "VM has finished installing"
-oc apply -f clone-boot-source.yaml
+${KC} apply -n ${IMAGES_NS} -f clone-boot-source.yaml
 echo "Applied DataVolume to clone boot source image"
 
 sleep 5
-dv_phase=$(oc -n openshift-virtualization-os-images get dv win2k19 -o jsonpath='{.status.phase}')
+dv_phase=$(${KC} -n ${IMAGES_NS} get dv win2k19 -o jsonpath='{.status.phase}')
 while [ "$dv_phase" != "Succeeded" ]
 do
     sleep 10
-    dv_phase=$(oc -n openshift-virtualization-os-images get dv win2k19 -o jsonpath='{.status.phase}')
+    dv_phase=$(${KC} -n ${IMAGES_NS} get dv win2k19 -o jsonpath='{.status.phase}')
     echo $dv_phase
 done
 
 echo "Cleaning up"
-oc delete -f windows-install-vm.yaml
+${KC} delete -f windows-install-vm.yaml
 
-my_app_name=$(oc get cm windows-install-scripts -o jsonpath='{.metadata.labels.app\.kubernetes\.io/instance}')
+my_app_name=$(${KC} get cm windows-install-scripts -o jsonpath='{.metadata.labels.app\.kubernetes\.io/instance}')
 
 echo "Finished"
